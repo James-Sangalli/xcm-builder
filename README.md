d80c8b520ce843039a2a8743078c6366454a4d2e
James Sangalli <j.l.sangalli@gmail.com>
2022-10-05 12:36:18 +1100

    refactor and document

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# xcm-module-sample
-Experimenting with XCM
+# xcm-builder
+Experimenting with XCM transfers from one chain to another.
+
+## Getting started 
+1. Install the dependencies: `$ npm i`.
+2. Follow the instructions [here](https://github.com/apopiak/execute-xcm-parachain) to build two private network nodes.
+3. Run the transfer with `$npm run start`.
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const { ApiPromise, WsProvider} = require("@polkadot/api");
-const endpoint = "ws://localhost:9988";
+const {
+    ApiPromise,
+    WsProvider
+} = require("@polkadot/api");
+const endpoint2000 = "ws://127.0.0.1:9988";
+const endpoint3000 = "ws://127.0.0.1:9999";
 
 // this function does a simple transfer of an asset from one parachain to another
 // https://polkadot.js.org/apps/?rpc=wss%3A%2F%2Fbasilisk-rpc.dwellir.com#/extrinsics/decode/0x34030208000400010200e12e0500000b00e057eb481b0e010004010100411f081300010200e12e050000070010a5d4e80107001c040a5d0d010004000101008eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48001c040a5d000000
@@ -11,14 +15,155 @@ async function transfer(
     recipient
 ) {
     const api = await init();
-    const xcmTransferSample = {callIndex: 0x3403, args: {message: {"v2":[{"withdrawAsset":[{"id":{"concrete":{"parents":1,"interior":{"x2":[{"parachain":fromParachain},{"generalIndex":0}]}}},"fun":{"fungible":amount}}]},{"depositReserveAsset":{"assets":{"wild":{"all":null}},"maxAssets":1,"dest":{"parents":1,"interior":{"x1":{"parachain":toParachain}}},"xcm":[{"buyExecution":{"fees":{"id":{"concrete":{"parents":1,"interior":{"x2":[{"parachain":3000},{"generalIndex":0}]}}},"fun":{"fungible":1000000000000}},"weightLimit":{"limited":399600000000}}},{"depositAsset":{"assets":{"wild":{"all":null}},"maxAssets":1,"beneficiary":{"parents":0,"interior":{"x1":{"accountId32":{"network":{"any":null},"id":recipient}}}}}}]}}]}, max_weight: 399600000000}};
+    const data = {
+        callIndex: getPolkadotXcmCallIndex(),
+        args: {
+            message: {
+                "v2": [
+                    getWithdrawAssetXCM(fromParachain, amount),
+                    getDepositReserveAssetXCM(toParachain, recipient)
+                ]
+            },
+            max_weight: getTransferMaxWeightEstimate()
+        }
+    };
 
-    return api.tx.polkadotXcm.execute({ V2: xcmTransferSample.args.message.v2 }, xcmTransferSample.args.max_weight).signAndSend(fromAccount);
+    return api.tx.polkadotXcm.execute({
+        V2: data.args.message.v2
+    }, data.args.max_weight).signAndSend(fromAccount);
+}
+
+/*
+* @dev gets the call index for polkadotXcm.execute
+* @returns - the call index
+* */
+function getPolkadotXcmCallIndex() {
+    return 0x3403;
+}
+
+/*
+* @dev gets the max weight estimate for the call
+* @returns - the hardcoded max weight estimate
+* */
+function getTransferMaxWeightEstimate() {
+    return 399600000000;
+}
+
+/*
+* @dev withdraw the asset from the origin chain so that it can be transferred to the destination
+* @param fromParachain - the parachainId where the asset lives
+* @param amount - the amount of the native fungible asset to withdraw
+* @returns - an XCM withdrawAsset object
+* */
+function getWithdrawAssetXCM(fromParachain, amount) {
+    return {
+        "withdrawAsset": [{
+            "id": {
+                "concrete": {
+                    "parents": 1,
+                    "interior": {
+                        "x2": [{ // this relates to how this token is defined on the parachain, the same asset can have a different value here
+                            "parachain": fromParachain
+                        }, {
+                            "generalIndex": 0
+                        }]
+                    }
+                }
+            },
+            "fun": {
+                "fungible": amount
+            }
+        }]
+    };
+}
+
+/*
+* @dev get the hardcoded fee for the transfer (this is based off of a BSX/HydraDx transfer)
+* @returns - the hardcoded fee
+* */
+function getTransferFee() {
+    return 1000000000000;
+}
+
+/*
+* @dev deposit the asset from the origin chain to the destination chain
+* @param toParachain - the parachainId where the asset will be deposited to
+* @param recipient - the receiver of the asset
+* @returns - an XCM depositReserveAsset object
+* */
+function getDepositReserveAssetXCM(toParachain, recipient) {
+    return {
+        "depositReserveAsset": {
+            "assets": {
+                "wild": {
+                    "all": null
+                }
+            },
+            "maxAssets": 1,
+            "dest": {
+                "parents": 1,
+                "interior": {
+                    "x1": {
+                        "parachain": toParachain
+                    }
+                }
+            },
+            "xcm": [{
+                "buyExecution": {
+                    "fees": {
+                        "id": {
+                            "concrete": {
+                                "parents": 1,
+                                "interior": {
+                                    "x2": [{
+                                        "parachain": 3000
+                                    }, {
+                                        "generalIndex": 0
+                                    }]
+                                }
+                            }
+                        },
+                        "fun": {
+                            "fungible": getTransferFee()
+                        }
+                    },
+                    "weightLimit": {
+                        "limited": getTransferMaxWeightEstimate()
+                    }
+                }
+            }, {
+                "depositAsset": {
+                    "assets": {
+                        "wild": {
+                            "all": null
+                        }
+                    },
+                    "maxAssets": 1,
+                    "beneficiary": {
+                        "parents": 0,
+                        "interior": {
+                            "x1": {
+                                "accountId32": {
+                                    "network": {
+                                        "any": null
+                                    },
+                                    "id": recipient
+                                }
+                            }
+                        }
+                    }
+                }
+            }]
+        }
+    };
 }
 
 async function init() {
-    const api = ApiPromise.create({ provider: new WsProvider(endpoint) });
+    const api = await ApiPromise.create({
+        provider: new WsProvider(endpoint3000)
+    });
     // https://github.com/galacticcouncil/Basilisk-node/blob/532cd08b0fc5bc936e34580239b58139b1553bb0/integration-tests/src/cross_chain_transfer.rs#L162
+    // for an asset to be recognised on the other chain, it must be registered.
     await api.tx.assetRegistry.setLocation(1, {
         parents: 0,
         interior: {
@@ -33,7 +178,9 @@ async function init() {
         }
     }).signAndSend("0x8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48");
 
-    return api;
+    return ApiPromise.create({
+        provider: new WsProvider(endpoint2000)
+    });
 }
 
 transfer("0x8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48", 3000, 2000, 30000000000000, "0x8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48")
